19b7f0e667b960fcb9c9f9108447b35f5160e58d
Alexander Smith <alexander.smith@just-eat.com>
2017-09-08 18:29:50 +0100

    Completed list view for iOS

---
+++ MvvmCrossDemo.iOS/Views/CustomCells/NotesViewCell.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Foundation;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.Binding.iOS.Views;
+using MvvmCrossDemo.Core.ViewModels;
+using UIKit;
+
+namespace MvvmCrossDemo.iOS.Views.CustomCells
+{
+	public partial class NotesViewCell : MvxTableViewCell
+	{
+		public static readonly NSString Key = new NSString("NotesViewCell");
+		public static readonly UINib Nib;
+
+		static NotesViewCell()
+		{
+			Nib = UINib.FromName("NotesViewCell", NSBundle.MainBundle);
+		}
+
+		protected NotesViewCell(IntPtr handle) : base(handle)
+		{
+			this.DelayBind(() =>
+			{
+				var set = this.CreateBindingSet<NotesViewCell, NotesCellViewModel>();
+				set.Bind(TitleLabel).To(vm => vm.Title);
+				set.Bind(SummaryLabel).To(vm => vm.Summary);
+				set.Apply();
+			});
+		}
+	}
+}
--- MvvmCrossDemo.iOS/Views/NotesView.cs
+++ MvvmCrossDemo.iOS/Views/NotesView.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.Binding.iOS.Views;
 using MvvmCross.iOS.Views;
+using MvvmCrossDemo.Core.ViewModels;
+using MvvmCrossDemo.iOS.Views.CustomCells;
 using UIKit;
 
 namespace MvvmCrossDemo.iOS.Views
@@ -14,10 +18,22 @@ namespace MvvmCrossDemo.iOS.Views
 		{
 			base.ViewDidLoad();
 
-			//TODO - Bindings
-		}
+			EdgesForExtendedLayout = UIRectEdge.None;
 
-		//TODO - implement table view
+			var source = new MvxSimpleTableViewSource(TableView, "NotesViewCell", NotesViewCell.Key);
+			TableView.RowHeight = 60;
+
+			var set = this.CreateBindingSet<NotesView, NotesViewModel>();
+			set.Bind(AddNoteButton).To(vm => vm.CreateNewNote);
+			set.Bind(source).For(x => x.SelectionChangedCommand).To(vm => vm.OnItemClicked);
+			set.Bind(source).To(vm => vm.Notes);
+			set.Apply();
+
+			source.DeselectAutomatically = true;
+
+			TableView.Source = source;
+			TableView.ReloadData();
+		}
 	}
 }
 
