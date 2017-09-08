22e756294a6e1fa74dc35b87378f6cc63e340a93
Alexander Smith <alexander.smith@just-eat.com>
2017-09-08 10:33:51 +0100

    Added core test project

---
+++ MvvmCrossDemo.Core.Tests/ViewModels/LoginViewModelTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Moq;
+using MvvmCrossDemo.Core.Services;
+using MvvmCrossDemo.Core.ViewModels;
+using NUnit.Framework;
+
+namespace MvvmCrossDemo.Core.Tests.ViewModels
+{
+	[TestFixture]
+	public class LoginViewModelTests
+	{
+		Mock<ILoginService> _loginService;
+
+		[SetUp]
+		public void TestSetUp()
+		{
+			_loginService = new Mock<ILoginService>();
+		}
+
+		[Test]
+		public void OnLoginFailureResetTextFields()
+		{
+			_loginService.Setup(x => x.TryLogin("user", "password")).Returns(false);
+
+			var ViewModel = CreateViewModel();
+			ViewModel.Username = "user";
+			ViewModel.Password = "password";
+
+			ViewModel.LoginCommand.Execute(this);
+
+			Assert.That(ViewModel.Username, Is.EqualTo(""));
+			Assert.That(ViewModel.Password, Is.EqualTo(""));
+		}
+
+		LoginViewModel CreateViewModel()
+		{
+			return new LoginViewModel(_loginService.Object);
+		}
+
+	}
+}
--- MvvmCrossDemo.Core/Services/LoginService.cs
+++ MvvmCrossDemo.Core/Services/LoginService.cs
@@ -1,12 +1,9 @@
 ﻿using System;
+
 namespace MvvmCrossDemo.Core.Services
 {
 	public class LoginService : ILoginService
 	{
-		public LoginService()
-		{
-		}
-
 		public bool TryLogin(string username, string password)
 		{
 			return username == password 
